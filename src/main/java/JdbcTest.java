5eb107d24a942640274862a17544c623d1bf70b0
shahzad.iqbal <shahzad.iqbal@bookassist.com>
2022-06-14 19:06:31 +0500

    adding hello world in main file

--- src/main/java/JdbcTest.java
+++ src/main/java/JdbcTest.java
@@ -10,8 +10,9 @@ public class JdbcTest {
     public static void main(String[] args) {
 
          new ViewStudent();
-        System.out.println("Hello world");
-        System.out.println("Hello world");
+        System.out.println("Hello world 1");
+        System.out.println("Hello world 2");
+        System.out.println("Hello world 3");
 
     }
 }
